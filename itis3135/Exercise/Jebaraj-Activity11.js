5e5e2010a0f1f8de3a0f0e804ca71c1a8523bdf7
jjebaraj <55603662+jjebaraj@users.noreply.github.com>
2022-05-05 23:03:36 -0400

    Case change

--- itis3135/Exercise/Jebaraj-Activity11.js
+++
@@ -1,8 +0,0 @@
-<!-- jQuery call to the accordion() method. -->
-$(document).ready(function() {
-    $( "#accordion" ).accordion({
-        active: 0,
-        heightStyle: "content",
-        collapsible: true,
-      });
-});
